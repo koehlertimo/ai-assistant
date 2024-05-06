414cd6b9314540c10eb3e9e501a74a76811f46db
Koehler, Timo (SE-A/35) <timo.koehler@volkswagen.de>
2024-05-06 18:09:17 +0200

    fixed issue so that answer and question can be longer than 255 chars

--- src/main/java/com/timokoehler/aiassistant/model/Message.java
+++ src/main/java/com/timokoehler/aiassistant/model/Message.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,7 +22,9 @@ public class Message {
     @Id
     @GeneratedValue
     private long id;
+    @Lob
     private String question;
+    @Lob
     private String answer;
     private LocalDateTime createdAt;
 
